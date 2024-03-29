d0fae569cbd6954a34f94a0a26d8f0d2d1a8bfb0
LAPTOP-NQLIGAA4\Admin <Thuongvuongdac15@gmail.com>
2024-03-29 19:37:23 +0700

    thuong - topic

---
+++ lib/screen_admin/a.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class A extends StatefulWidget {
+  const A({super.key});
+
+  @override
+  State<A> createState() => _AState();
+}
+
+class _AState extends State<A> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
