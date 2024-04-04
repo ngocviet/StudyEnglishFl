f6d577032afc3db3df84116103cf46af3b309f87
DoTienVan <dvtien238@gmail.com>
2024-04-04 20:35:04 +0700

    t

--- lib/screen/combine_sentences_screen.dart
+++ lib/screen/combine_sentences_screen.dart
@@ -12,6 +12,7 @@ class CombineSentencesScreen extends StatefulWidget {
 
 class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
   late modelCombineSentences _data;
+  List<Word> newWord = [];
   int totalHeart = 3;
   int canSubmit = 0;
 
@@ -27,34 +28,57 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
       return Visibility(
         visible: word.isShowText,
         child: ElevatedButton(
-            key: UniqueKey(),
-            onPressed: () {
-              setState(() {
-                // _data = CombineSentencesController.getData1();
-                word.isShowText = !word.isShowText;
-                canSubmit ++;
-              });
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromRGBO(2, 32, 47, 1.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-              ),
-              side: const BorderSide(color: Color.fromRGBO(
-                  145, 161, 189, 1.0), width: 2),
+          key: UniqueKey(),
+          onPressed: () {
+            setState(() {
+              word.isShowText = !word.isShowText;
+              canSubmit++;
+              newWord.add(word);
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromRGBO(2, 32, 47, 1.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
             ),
-            child:
-            Padding(
-              padding: const EdgeInsets.fromLTRB(5,10,5,10),
-              child: Text(
-                word.nameEn,
-                style: const TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            )
-
+            side: const BorderSide(
+              color: Color.fromRGBO(145, 161, 189, 1.0),
+              width: 2,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+            child: Text(
+              word.nameEn,
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
         ),
       );
     }).toList();
+
+    List<Widget> newWordWidgets = newWord.map((word) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            word.isShowText = !word.isShowText;
+            newWord.remove(word); // Xóa từ khỏi danh sách newWord
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(right: 8),
+          child: Text(
+            word.nameEn,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -69,36 +93,36 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
           children: [
             Expanded(
               child: Stack(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 18,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 18,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(197, 206, 215, 1.0),
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 18,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(22, 182, 32, 1.0),
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                  ),
+                  Positioned(
+                    top: 4,
+                    left: 15,
+                    child: Container(
+                      width: 70,
+                      height: 6,
                       decoration: BoxDecoration(
-                          color: const Color.fromRGBO(197, 206, 215, 1.0),
-                          borderRadius: BorderRadius.circular(9)
+                        color: const Color.fromRGBO(255, 255, 255, 0.3),
+                        borderRadius: BorderRadius.circular(3),
                       ),
                     ),
-                    Container(
-                      width: 100,
-                      height: 18,
-                      decoration: BoxDecoration(
-                          color: const Color.fromRGBO(22, 182, 32, 1.0),
-                          borderRadius: BorderRadius.circular(9)
-                      ),
-                    ),
-                    Positioned(
-                      top: 4,
-                      left: 15,
-                      child: Container(
-                        width: 70,
-                        height: 6,
-                        decoration: BoxDecoration(
-                            color: const Color.fromRGBO(255, 255, 255, 0.3),
-                            borderRadius: BorderRadius.circular(3)
-                        ),
-                      ),
-                    ),
-                  ]
+                  ),
+                ],
               ),
             ),
             Row(
@@ -115,19 +139,18 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
                         color: Colors.red,
                         iconSize: 30,
                       ),
-
                     ),
                     Positioned(
                       top: 16,
-                        left: 16,
-                        child: Container(
-                          width: 6,
-                          height: 6,
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255, 255, 255, 0.3),
-                            borderRadius: BorderRadius.circular(3),
-                          ),
-                        )
+                      left: 16,
+                      child: Container(
+                        width: 6,
+                        height: 6,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.3),
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
                     )
                   ],
                 ),
@@ -136,7 +159,7 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
                   style: const TextStyle(
                     color: Colors.red,
                     fontSize: 22,
-                    fontWeight: FontWeight.bold
+                    fontWeight: FontWeight.bold,
                   ),
                 )
               ],
@@ -192,8 +215,10 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
                           padding: const EdgeInsets.only(bottom: 4),
                           decoration: const BoxDecoration(
                             border: Border(
-                              bottom: BorderSide(color: Color.fromRGBO(
-                                  136, 136, 136, 1.0), width: 1),
+                              bottom: BorderSide(
+                                color: Color.fromRGBO(136, 136, 136, 1.0),
+                                width: 1,
+                              ),
                             ),
                           ),
                           child: Text(
@@ -219,56 +244,29 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
                                 width: 400,
                                 child: Wrap(
                                   runSpacing: 30,
-                                  children: List.generate(
-                                    _data.listWord.length,
-                                        (index) {
-                                      Word word = _data.listWord[index];
-                                      return Visibility(
-                                        visible: !word.isShowText,
-                                        child: GestureDetector(
-                                          onTap: (){
-                                            setState(() {
-                                              word.isShowText = !word.isShowText;
-                                              canSubmit --;
-                                            });
-                                          },
-                                          child: Padding(
-                                            padding: const EdgeInsets.only(right: 8),
-                                            child: Text(
-                                              word.nameEn,
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 20,
-                                                decoration: TextDecoration.underline,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
+                                  children: newWordWidgets,
                                 ),
                               ),
                             ),
                             Positioned(
                               top: 60,
-                                child: Container(
-                                  height: 1,
-                                  width: 400,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.grey
-                                  ),
-                                )
+                              child: Container(
+                                height: 1,
+                                width: 400,
+                                decoration: const BoxDecoration(
+                                  color: Colors.grey,
+                                ),
+                              ),
                             ),
                             Positioned(
                               top: 120,
-                                child: Container(
-                                  height: 1,
-                                  width: 600,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.grey
-                                  ),
-                                )
+                              child: Container(
+                                height: 1,
+                                width: 600,
+                                decoration: const BoxDecoration(
+                                  color: Colors.grey,
+                                ),
+                              ),
                             )
                           ],
                         ),
@@ -292,24 +290,28 @@ class _CombineSentencesScreenState extends State<CombineSentencesScreen> {
                 child: SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: canSubmit >= 2 ? const Color.fromRGBO(67, 203, 47,1.0) : const Color.fromRGBO(
-                            94, 94, 94, 1.0),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: canSubmit >= 2
+                          ? const Color.fromRGBO(67, 203, 47, 1.0)
+                          : const Color.fromRGBO(94, 94, 94, 1.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(
+                        'Kiểm tra',
+                        style: TextStyle(
+                          color: canSubmit >= 2
+                              ? Colors.black
+                              : const Color.fromRGBO(134, 134, 134, 1.0),
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      onPressed: () {
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: Text(
-                          'Kiểm tra',
-                          style: TextStyle(
-                              color: canSubmit >= 2 ? Colors.black : const Color.fromRGBO(134, 134, 134, 1.0),
-                              fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                      )
+                    ),
                   ),
                 ),
               ),
